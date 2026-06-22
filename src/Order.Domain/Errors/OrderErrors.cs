0b5ee512df112fda41953aec1311b526831a3cc5
Teya- <sobkalovt@bk.ru>
2026-06-22 23:01:50 +0300

    feat(domain): add order entity

---
+++ src/Order.Domain/Errors/OrderErrors.cs
@@ -0,0 +1,11 @@
+﻿using Order.Domain.Common;
+
+namespace Order.Domain.Errors;
+
+public static class OrderErrors
+{
+    public static readonly Error InvalidCityName = new Error("City name should contain from 1 to 168 symbols.", "Order.InvalidCityName");
+    public static readonly Error InvalidAddressName = new Error("Address name should contain from 4 to 100 symbols.", "Order.InvalidAddressName");
+    public static readonly Error InvalidWeight = new Error("Weight must be greater than 0 and less than or equal to 1500 kg.", "Order.InvalidWeight");
+    public static readonly Error InvalidPickUpDate = new Error("Pick up date can't be earlier than the current time.", "Order.InvalidPickUpDate");
+}
