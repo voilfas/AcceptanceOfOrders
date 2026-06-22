8ebb32a8796086a120128f63276a0a607807d67c
Teya- <sobkalovt@bk.ru>
2026-06-23 02:26:06 +0300

    feat(application): implement get order by id query

---
+++ src/OrderService.Application/UseCases/Queries/GetOrderById/GetOrderByIdDto.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderService.Application.UseCases.Queries.GetOrderById;
+
+public record GetOrderByIdDto(
+    Guid Id,
+    string OrderNumber,
+    string SenderCity,
+    string SenderAddress,
+    string ReceiverCity,
+    string ReceiverAddress,
+    decimal Weight,
+    DateTime PickUpDate,
+    DateTime CreatedAt);
