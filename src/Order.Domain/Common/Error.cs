568a813e77d6c4423168d0ea4c5ec73acc272a00
Teya- <sobkalovt@bk.ru>
2026-06-22 21:48:55 +0300

    feat(domain): implement result pattern

---
+++ src/Order.Domain/Common/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace Order.Domain.Common;
+
+public record Error
+{
+    public string Message { get; }
+    public string Code { get; }
+
+    public Error(string message, string code)
+    {
+        Message = message;
+        Code = code;
+    }
+}
---
+++ src/Order.Domain/Common/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace Order.Domain.Common;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+
+    protected Result(bool isSuccess, Error? error)
+    {
+        if (isSuccess && error is not null)
+            throw new ArgumentException("Success and Error!");
+        
+        if (!isSuccess && error is null)
+            throw new ArgumentException("Not Success and Not Error!");
+        
+        IsSuccess = isSuccess;
+        Error = error!;
+    }
+
+    public static Result Success() => new Result(true, null);
+    
+    public static Result Failure(Error error) => new Result(false, error);
+}
