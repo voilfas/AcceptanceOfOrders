79eb99b96a33a5ef40b7beeffd7f6db2b97f8bcc
Teya- <sobkalovt@bk.ru>
2026-06-23 02:28:03 +0300

    feat(application): implement interface order repository

---
+++ src/OrderService.Application/Interfaces/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using OrderService.Domain.Entities;
+
+namespace OrderService.Application.Interfaces;
+
+public interface IOrderRepository
+{
+    Task AddAsync(Order order, CancellationToken cancellationToken);
+    
+    Task<Order?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    
+    Task<List<Order>> GetAllAsync(CancellationToken cancellationToken);
+    
+    Task SaveAsync(CancellationToken cancellationToken);
+}
