d8831f22bdf8af48ef5069d1b9ee16f090ca2e11
Teya- <sobkalovt@bk.ru>
2026-06-23 02:25:11 +0300

    feat(application): implement create order command

---
+++ src/OrderService.Application/UseCases/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using OrderService.Domain.Common;
+
+namespace OrderService.Application.UseCases.Commands.CreateOrder;
+
+public record CreateOrderCommand(
+    string SenderCity,
+    string SenderAddress,
+    string ReceiverCity,
+    string ReceiverAddress,
+    decimal Weight,
+    DateTime PickUpDate) : IRequest<Result<string>>;
---
+++ src/OrderService.Application/UseCases/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using OrderService.Application.Interfaces;
+using OrderService.Domain.Common;
+using OrderService.Domain.Entities;
+
+namespace OrderService.Application.UseCases.Commands.CreateOrder;
+
+public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Result<string>>
+{
+    private readonly IOrderRepository _repository;
+
+    public CreateOrderCommandHandler(IOrderRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public async Task<Result<string>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var resultOrder = Order.Create(
+            request.SenderCity,
+            request.SenderAddress,
+            request.ReceiverCity,
+            request.ReceiverAddress,
+            request.Weight,
+            request.PickUpDate);
+
+        if (resultOrder.IsFailure)
+            return Result<string>.Failure(resultOrder.Error);
+        
+        var order = resultOrder.Value!;
+        
+        await _repository.AddAsync(order, cancellationToken);
+        await _repository.SaveAsync(cancellationToken);
+        
+        return Result<string>.Success(order.OrderNumber);
+    }
+}
