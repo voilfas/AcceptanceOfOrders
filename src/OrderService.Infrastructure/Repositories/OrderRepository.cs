d5b7052d0e36da20485f435b4c202592ca96b4d7
Teya- <sobkalovt@bk.ru>
2026-06-23 16:17:08 +0300

    feat(infrastructure): implement order repository

---
+++ src/OrderService.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Application.Interfaces;
+using OrderService.Domain.Entities;
+
+namespace OrderService.Infrastructure.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly OrderDbContext _context;
+    
+    public OrderRepository(OrderDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddAsync(Order order, CancellationToken cancellationToken)
+    {
+        await _context.AddAsync(order, cancellationToken);
+    }
+
+    public async Task<Order?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.FindAsync<Order>(id, cancellationToken);
+    }
+
+    public async Task<List<Order>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _context.Orders
+            .OrderByDescending(x => x.CreatedAt)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task SaveAsync(CancellationToken cancellationToken)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
