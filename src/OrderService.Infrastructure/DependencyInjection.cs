c43bffd0f1662e306d75032b2aa91d17c0f70201
Teya- <sobkalovt@bk.ru>
2026-06-24 17:33:56 +0300

    feat(infrastructure): rigister repository

---
+++ src/OrderService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderService.Application.Interfaces;
+using OrderService.Infrastructure.Repositories;
+
+namespace OrderService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        
+        return services;
+    }
+}
